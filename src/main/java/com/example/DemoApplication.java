e790f7e9cc11729ddc1687566840c441b0b404c1
Robert Angus <rmangus@winsupplyinc.com>
2016-07-28 11:35:55 -0400

    poc1

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "hopefully!! this is branch POC-1";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
