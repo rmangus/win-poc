1e84f232334ec0dc92919280e7be9398dbafc747
Robert Angus <rmangus@winsupplyinc.com>
2016-08-04 16:49:25 -0400

    whaaaaat

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,11 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.stereotype.*;
+
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
@@ -16,17 +14,33 @@ import java.sql.*;
 @SpringBootApplication
 public class DemoApplication {
 
-	@Autowired
+    @Autowired
     private DataSource dataSource;
-    
+
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-      return "welp";
+    String home() throws SQLException {
+        myRealMainMethod();
+        return "more testing";
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void myRealMainMethod() throws SQLException {
+        try {
+            Statement stmt = dataSource.getConnection().createStatement();
+            stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+            stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+            while (rs.next()) {
+                System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 }
