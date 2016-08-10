442524e9c42b811dc6a8dc9cdd57a1f0aaca3858
Robert Angus <rmangus@winsupplyinc.com>
2016-08-10 13:14:50 -0400

    switching to jdbctemplate

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -1,41 +1,30 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
-import java.util.Arrays;
 
 @RestController
 public class TestController {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    /*
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-    */
-
-    @RequestMapping("/")
-    public static String readFromDB() throws Exception {
+    public String readFromDB() throws Exception {
         String junk = "junk";
-        Connection connection = getConnection();
 
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-        while (rs.next()) {
-            junk = rs.getTimestamp("tick").toString();
-            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+        jdbcTemplate.execute("DROP TABLE IF EXISTS ticks");
+        jdbcTemplate.execute("CREATE TABLE ticks (tick timestamp)");
+        jdbcTemplate.execute("INSERT INTO ticks VALUES (now())");
+        SqlRowSet srs = jdbcTemplate.queryForRowSet("SELECT tick FROM ticks");
+        while (srs.next()) {
+            junk = srs.getTimestamp("tick").toString();
+            System.out.println("Read from DB: " + srs.getTimestamp("tick"));
         }
 
         return junk;
