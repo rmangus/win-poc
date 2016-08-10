3a166a15077e587c5e6f9e2ba56dd2fe6eb36c17
Robert Angus <rmangus@winsupplyinc.com>
2016-08-10 13:33:17 -0400

    messing around with table schema

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -8,34 +8,25 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
+import java.util.Hashtable;
 
 @RestController
 public class TestController {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @RequestMapping("/")
-    public String readFromDB() throws Exception {
-        String junk = "junk";
+    @RequestMapping(path = "/", produces = "application/json")
+    public Hashtable<Integer, String> readFromDB() throws Exception {
+        Hashtable<Integer, String> junk = new Hashtable<Integer, String>();
 
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS ticks (SERIAL id PRIMARY KEY, tick timestamp)");
         jdbcTemplate.execute("INSERT INTO ticks VALUES (now())");
-        SqlRowSet srs = jdbcTemplate.queryForRowSet("SELECT tick FROM ticks");
+        SqlRowSet srs = jdbcTemplate.queryForRowSet("SELECT id, tick FROM ticks");
         while (srs.next()) {
-            junk = srs.getTimestamp("tick").toString();
-            System.out.println("Read from DB: " + srs.getTimestamp("tick"));
+            junk.put(srs.getInt("id"), srs.getTimestamp("tick").toString());
+            System.out.println("Read from DB: " + srs.getInt("id") + " : " + srs.getTimestamp("tick"));
         }
 
         return junk;
-    }
-
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
