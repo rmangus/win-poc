4ea672e6e4122ec36bf82f4f44dafc9d53614be3
Robert Angus <rmangus@winsupplyinc.com>
2016-08-10 11:54:03 -0400

    well it builds now i guess

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,47 +1,14 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.context.annotation.*;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.sql.*;
-
-@Controller
+@Configuration
 @SpringBootApplication
-@PropertySource({"classpath:application.properties"})
 public class DemoApplication {
-    @Autowired
-    private DataSource dataSource;
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() throws SQLException {
-        myRealMainMethod();
-        return "more testing";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @PostConstruct
-    public void myRealMainMethod() throws SQLException {
-        try {
-            Statement stmt = dataSource.getConnection().createStatement();
-            stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-            stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-            while (rs.next()) {
-                System.out.println("Read from DB: " + rs.getTimestamp("tick"));
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
     }
 }
