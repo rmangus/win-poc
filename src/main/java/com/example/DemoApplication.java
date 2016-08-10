5a86bc13126b2a6f2c0879e8657917b048963cd4
Robert Angus <rmangus@winsupplyinc.com>
2016-08-10 11:01:22 -0400

    maybe it works now

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
@@ -12,8 +13,8 @@ import java.sql.*;
 
 @Controller
 @SpringBootApplication
+@PropertySource({"classpath:application.properties"})
 public class DemoApplication {
-
     @Autowired
     private DataSource dataSource;
 
