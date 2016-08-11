2e0794a8e9623cbe9d15e18249d5bb8b17e76268
Robert Angus <rmangus@winsupplyinc.com>
2016-08-11 09:40:21 -0400

    added dbconfig

---
+++ src/main/java/com/example/DatabaseConfig.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactory.setDataSource(jdbcTemplate.getDataSource());
+
+        // Classpath scanning of @Component, @Service, etc annotated class
+        entityManagerFactory.setPackagesToScan(env.getProperty("entitymanager.packagesToScan"));
+
+        // Vendor adapter
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+
+        // Hibernate properties
+        Properties additionalProperties = new Properties();
+        additionalProperties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        additionalProperties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        additionalProperties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        entityManagerFactory.setJpaProperties(additionalProperties);
+
+        return entityManagerFactory;
+    }
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+}
